af13d65e4f9ddac5d3f5bbd5bb54a8766ead1ff3
felipe <feli.rego1@gmail.com>
2025-08-13 14:29:30 -0300

    commit develop 1

--- repaso.py
+++ repaso.py
@@ -3,7 +3,10 @@ from metodos import insertar_numero
 lista_numeros=[]
 
 for i in range(3):
-    numero=input("ingrese un numero")
+    numero=int(input("ingrese un numero"))
+    if numero > 9:
+        print(f"no se admiten numeros mayores a 9")
+        continue
     insertar_numero (lista_numeros, numero)
 print(f"lista de numeros: {lista_numeros}")
 
