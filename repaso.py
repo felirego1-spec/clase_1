67ee507d5c18375e6a0b83a91b2c1d2b9d559ba2
felipe <feli.rego1@gmail.com>
2025-08-13 14:54:01 -0300

    commit develop 2

--- repaso.py
+++ repaso.py
@@ -2,11 +2,10 @@ from metodos import insertar_numero
 
 lista_numeros=[]
 
-for i in range(3):
+for i in range(5):
     numero=int(input("ingrese un numero"))
-    if numero > 9:
-        print(f"no se admiten numeros mayores a 9")
-        continue
+    if numero %2 !=0 :
+        continue   #este continue lo que me dice es que se continua con la iteracion osea ocupa un lugar de los 3 pero no se agrega a la lista ese valor mayor que 9
     insertar_numero (lista_numeros, numero)
 print(f"lista de numeros: {lista_numeros}")
 
